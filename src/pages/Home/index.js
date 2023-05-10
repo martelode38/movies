3f25edc7b9fb910a4da4d37480c2393d2ee6a707
martelode38 <marteniers@gmail.com>
2023-05-10 11:44:58 -0300

    aula 11, revisnado o useState

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Category, {categories, filterCategory} from "../../components/Category";
@@ -11,6 +12,17 @@ import Header from "../../components/Header";
 
 function Home() {
   
+  const [nome, setNome] = useState()
+  
+  let NomeDigitado =''
+
+  function pegarNome(e){
+    console.log(e.target.value)
+    setNome(e.target.value)
+    
+
+  }
+
 
   return (
     <div>
@@ -19,14 +31,24 @@ function Home() {
         <Banner image ="home"/>
 
         <Container>
-          {
+
+          <input
+            type="text"
+            placeholder="nome"
+            onChange={pegarNome}
+
+          />
+          <h2>{nome}</h2>
+
+          {/* {
             categories.map((category, index)=>
             <Category category={category}>
         
           {filterCategory(index).map((video)=> <Card id={video.id} key={video.id}/>)}
             </Category>)
             
-          }
+          } */}
+          
 
         </Container>
        
